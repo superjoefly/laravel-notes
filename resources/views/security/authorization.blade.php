d098a715f66ff1c7d1ee8f2eb027d1b7006e2278
superjoefly <superjoefly@gmail.com>
2017-10-02 18:16:06 -0600

    Added notes and snippets for Creating Policies.

--- resources/views/security/authorization.blade.php
+++ resources/views/security/authorization.blade.php
@@ -93,5 +93,56 @@
 
   <h3>Generating Policies</h3>
 
+  <p>Policies are classes that organize authorization logic around a particular model or resource. For example, if creating a blog application, we could have a Post model and a corresponding PostPolicy to authorize user actions such as creating or updating posts.</p>
+
+  <p>We can generate a policy using the make:policy artisan command. The generated policy will be placed in the app/Policies directory:</p>
+
+  <pre><code class="language-php">
+    php artisan make:policy PostPolicy
+  </code></pre>
+
+  <p>The make:policy command will generate an empty policy class. To create a policy with the basic CRUD policy methods already included in the class, we can specify a --model when executing the command:</p>
+
+  <pre><code class="language-php">
+    php artisan make:policy PostPolicy --model=Post
+  </code></pre>
+
+  <div class="w3-panel w3-border-blue w3-leftbar w3-pale-blue">
+    <p>All policies are resolved via the Laravel service container, allowing us to type-hint any needed dependencies in the policy's constructor to have them automatically injected.</p>
+  </div>
+
+  <h3>Registering Policies</h3>
+
+  <p>Once the policy exists, it needs to be registered. The AuthServiceProvider included with Laravel contains a policies property that maps the Eloquent models to their cooresponding policies. Registering a policy will tell Laravel which policy to use when authorizing actions against a given model.</p>
+
+  <pre><code class="language-php">
+    namespace App\Providers;
+
+    use App\Post;
+    use App\Policies\PostPolicy;
+    use Illuminate\Support\Facades\Gate;
+    use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+    class AuthServiceProvider extends ServiceProvider
+    {
+        protected $policies = [
+            Post::class => PostPolicy::class,
+        ];
+
+        public function boot()
+        {
+            $this->registerPolicies();
+
+            //
+        }
+    }
+  </code></pre>
+
+  <h2>Writing Policies</h2>
+
+  <h3>Policy Methods</h3>
+
   <p></p>
+
+
 @endsection
