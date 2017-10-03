0f81f0aabbdd539759e968a07e9d227314dfcc87
superjoefly <superjoefly@gmail.com>
2017-10-02 20:55:51 -0600

    Added notes and snippets for Writing Policies.

--- resources/views/security/authorization.blade.php
+++ resources/views/security/authorization.blade.php
@@ -142,7 +142,67 @@
 
   <h3>Policy Methods</h3>
 
-  <p></p>
+  <p>Once the policy has been registered, we can add methods for each action it authorizes. In the next example, we'll define an update() method on our PostPolicy which determines if a given User can update a given Post instance.</p>
 
+  <p>The update() method will receive a User and a Post instance as its arguments, and should return true or false indicating whether a user is authorized to update the given Post. Here, we'll verify that the user's id matches the user_id on the post:</p>
+
+  <pre><code class="language-php">
+    namespace App\Policies;
+
+    use App\User;
+    use App\Post;
+
+    class PostPolicy
+    {
+        public function update(User $user, Post $post)
+        {
+            return $user->id === $post->user_id;
+        }
+    }
+  </code></pre>
+
+  <p>We can continue defining additional methods on the policy as needed for the various actions it authorizes. For example, we can define view() or delete() methods to authorize various Post actions.</p>
+
+  <div class="w3-panel w3-border-blue w3-leftbar w3-pale-blue">
+    <p>When using the --model option on the Artisan make:policy command, the generated policy will already contain methods for the view, create, update and delete actions.</p>
+  </div>
+
+  <h3>Methods Without Models</h3>
+
+  <p>Some policy methods only receive the currently authenticated user and not an instance of the model they authorize. This is most common when authorizing create actions. For example, when creating a blog, we may want to check if a user is authorized to create any posts at all.</p>
+
+  <p>When defining policy methods that do not receive a model instance, such as the create method, we should define the method as only expecting the authenticated user:</p>
+
+  <pre><code class="language-php">
+    public function create(User $user)
+    {
+        //
+    }
+  </code></pre>
+
+  <h3>Policy Filters</h3>
+
+  <p>For certain users, we may want to authorize all actions within a given policy. To do this, we can define a before() method on the policy. The before() method will be executed before any other methods on the policy, giving us an opportunity to authorize the action before the intended policy method is actually called. This feature is most commonly used for authorizing admins to perform any action:</p>
+
+  <pre><code class="language-php">
+    public function before($user, $ability)
+    {
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+    }
+  </code></pre>
+
+  <p>To deny all actions for a user, we should return false from the before() method. If null is returned, the authorization will fall through to the policy method.</p>
+
+  <div class="w3-panel w3-border-blue w3-leftbar w3-pale-blue">
+    <p>The before() method of a policy class will not be called if the class doesn't contain a method with a name matching the name of the ability being checked.</p>
+  </div>
+
+  <h2>Authorizing Actions Using Policies</h2>
+
+  <h3>Via the User Model</h3>
+
+  <p></p>
 
 @endsection
